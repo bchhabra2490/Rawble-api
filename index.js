3229873765cc4c0331bb1c16caf15bcc4f01de8f
Bharat Chhabra <bharat.c@solutionbeyond.net>
2019-12-15 18:30:51 +0530

    Added packages

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import indexRouter from './routes/index'
+import path from 'path';
+import dotenv from 'dotenv';
+import domain from 'domain';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const d = domain.create()
+const app = express();
+var router = express.Router();
+const port = process.env.PORT || 3000;
+const dbUrl = process.env.DB_URL;
+const jwtSecretKey = process.env.jwtsecretkey;
+
+
+mongoose.connect(dbUrl, { useNewUrlParser: true })
+.then(() => {
+    console.log('mlab connection succesful');
+    console.log("Cron job started");
+}).catch(e=>{
+    console.log("Mongoose not connected", dbUrl);
+})
+mongoose.Promise = global.Promise;
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.use(function(req, res, next) {
+    // We lost connection!
+       if (1 !== mongoose.connection.readyState) {
+   
+           // Reconnect if we can
+           mongoose.connect(dbUrl,  { useNewUrlParser: true })
+               .then(() => {
+                   console.log('DB connection succesful')
+               })
+               .catch((err) => console.log("Mongoose not connected", dbUrl));
+           res.status(503).json({message: "Mongoose Not Connected"})
+           throw new Error('Mongo not available');
+       }
+     next();
+});
+
+
+router.get('/', function(req, res, next) {
+    res.json({ title: 'Express' });
+});
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.all('/', router);
+app.use('/api/v1', indexRouter);
+
+app.listen(port);
